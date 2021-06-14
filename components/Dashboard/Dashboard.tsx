004470f141e1f5c173565b1b07d9e0355a4d8fa2
Erik Beem <erik.beem@hotmail.com>
2021-06-14 11:36:40 +0200

    Added useSWR hook for fetching data

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,69 +1,28 @@
-import { useState, useEffect } from "react";
 import { CoinList } from "../CoinList/CoinList";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./Dashboard.module.css";
-import { Coin as CoinT } from "../../interfaces/Coin.d";
-import axios from "axios";
 
-function Dashboard({ ...props }) {
-  const [coins, setCoins] = useState([]);
-  const [coinsFetched, setCoinsFetched] = useState(false);
-  const [coinsFetchedError, setCoinsFetchedError] = useState(null);
+import { useSWRFetch } from "../util/fetchData";
+import { useSocket } from "../useSocket/useSocket";
 
-  useEffect(() => {
-    axios
-      .get<CoinT[]>(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum%2C%20cardano%2C%20xrp%2C%20polkadot%2C%20litecoin%2C%20dash%2C%20chainlink%2C%20vechain%2C%20tron&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-      )
-      .then((res) => res.data)
-      .then(
-        (result) => {
-          setCoinsFetched(true);
-          setCoins(result);
-        },
-        (error) => {
-          setCoinsFetched(true);
-          setCoinsFetchedError(error);
-        }
-      );
-  }, []);
+function Dashboard() {
+  const { coins, isLoading, isError } = useSWRFetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum%2C%20cardano%2C%20xrp%2C%20polkadot%2C%20litecoin%2C%20dash%2C%20chainlink%2C%20vechain%2C%20tron&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+  const socket = useSocket('', cb => {
+    
+  })
+  if (isLoading) return <div>Loading</div>
 
-  if (coinsFetched) {
-    console.log(coins);
-    return (
-      <div className={styles.Dashboard}>
-        <h2>Crypto-Dashboard</h2>
-        <p>Dashboard</p>
-        <SearchBar />
-        <CoinList coins={coins} />
-      </div>
-    );
-  } else if (!coinsFetchedError) {
-    return (
-      <>
-        <p>Error: {coinsFetchedError}</p>
-      </>
-    );
-  } else if (!coinsFetched) {
-    return (
-      <>
-        <h2>Loading...</h2>
-      </>
-    );
-  }
+  if (isError) return <div>Error {isError} </div>
+
+  if (coins) 
+  return (
+    <div className={styles.Dashboard}>
+      <h2>Crypto-Dashboard</h2>
+      <p>Dashboard</p>
+      <SearchBar />
+      <CoinList coins={coins} />
+    </div>
+  );
 }
-
-// export const GetServerSideProps = async () => {
-//     const res = await fetch("https://api.coingecko.com/api/v3/coins/cardano")
-//     const filteredCoins = await res.json()
-
-//     console.log(filteredCoins)
-
-//     return {
-//       props: {
-//         filteredCoins
-//       }
-//     }
-//   }
 
 export default Dashboard;
--- components/util/fetchData.tsx
+++ components/util/fetchData.tsx
@@ -1,15 +1,45 @@
 import axios from "axios";
-import { Coin as CoinT } from "../../interfaces/Coin.d";
+import useSWR from 'swr';
 
 // https://stackoverflow.com/questions/62217642/react-and-typescript-which-types-for-axios-response
 // https://github.com/axios/axios/blob/master/index.d.ts#L140
 // https://github.com/axios/axios#example
-export async function getData(url: string) {
+// ApiResponse generic interface: https://www.freecodecamp.org/news/make-typescript-easy-using-basic-ts-generics/
+
+interface ApiResponse<T> {
+  errorMessage?: string;
+  responseCode?: string;
+  data?: T;
+}
+
+export async function getDataAsync<T>(url: string): Promise<ApiResponse<T>> {
   try {
-    const res = await axios.get<CoinT[]>(url);
-    const data = res.data;
-    return data;
-  } catch (err) {
-    console.log(err.message);
+    const res = await axios.get(url);
+    if (res.status === 200) {
+      return res.data;
+    }
+    console.log(res.status);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function getDataSync<T>(url: string) {
+  axios.get<T>(url)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+const axiosFetcher = (url: string) => axios.get(url).then(res => res.data)
+
+export function useSWRFetch(url: string) {
+  const { data, error } = useSWR(url, axiosFetcher)
+
+  return {
+    coins: data,
+    isLoading: !error && !data,
+    isError: error,
   }
 }
