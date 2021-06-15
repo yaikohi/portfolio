6cc1576c5b5ee461256048e155d4ff856646494b
Erik Beem <erik.beem@hotmail.com>
2021-06-15 12:44:44 +0200

    ...

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,47 +1,52 @@
+// * https://github.com/renaissancetroll/reactjs-crypto-api-dashboard/blob/master/src/App.js
+// * for connecting to a coinbase websocket in a react project
+
+// using useRef hooks to update a variable without reloading the component: https://www.smashingmagazine.com/2020/11/react-useref-hook/
+import { useEffect, useState, useRef } from "react";
+import io from "socket.io";
+
 import { CoinList } from "../CoinList/CoinList";
-import SearchBar from "../SearchBar/SearchBar";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { useSWRFetch } from "../util/fetchData";
 import styles from "./Dashboard.module.css";
 
-import { useSWRFetch } from "../util/fetchData";
-import { useSocket } from "../useSocket/useSocket";
-
 function Dashboard() {
-  const { coins, isLoading, isError } = useSWRFetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum%2C%20cardano%2C%20xrp%2C%20polkadot%2C%20litecoin%2C%20dash%2C%20chainlink%2C%20vechain%2C%20tron&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-  );
-  const socket = useSocket({
-    wsUrl: "wss://ws-feed.pro.coinbase.com",
-    eventName: "jo",
-    callback: 0,
-  });
-  
-  // subscription to the coinbase websocket
-  const wsSub = {
-    type: "subscribe",
-    product_ids: ["ADA-EUR", "VET-EUR"],
-    channels: ["ticker"],
-  };
-  
-  socket.send(wsSub);
-  
-  // socket.onmessage = (e: Event) => {
-  //   let data;
-  //   if (data.type !== "ticker") {
-  //     return;
-  //   } 
-  //   if (data.product_id === pair) {
-  //     setprice(data.price);
-  //   }
-  // };
+  const apiUrl =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum%2C%20cardano%2C%20xrp%2C%20polkadot%2C%20litecoin%2C%20dash%2C%20chainlink%2C%20vechain%2C%20tron&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+  const wsUrl = "wss://ws-feed.pro.coinbase.com";
 
-  // unsubscribe message to stop the coinbase ws stream
-  const wsUnSub = {
-    type: "unsubscribe",
-    product_ids: ["ADA-EUR", "VET-EUR"],
-    channels: ["ticker"],
-  };
+  const socket = useRef(null);
+
+  const { coins, isLoading, isError } = useSWRFetch(apiUrl);
+
+  useEffect(() => {
+    socket.current = io.on(wsUrl, (socket) => {
+      console.log(socket);
+    });
+  }, []);
+
+  useEffect(() => {
+    // subscription to the coinbase websocket
+    const wsSubMsg = {
+      type: "subscribe",
+      product_ids: ["ADA-EUR", "VET-EUR"],
+      channels: ["ticker"],
+    };
+
+    socket.current.send(wsSubMsg);
+    socket.current.onmessage((event: Event) => {
+      console.log(event);
+    });
+
+    let wsUnsubMsg = {
+      type: "unsubscribe",
+      product_ids: ["ADA-EUR", "VET-EUR"],
+      channels: ["ticker"]
+    };
+    socket.current.send(wsUnsubMsg);
 
 
+  }, []);
 
   if (isLoading) return <div>Loading</div>;
 
