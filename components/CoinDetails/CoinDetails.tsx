89f95da17c7d23cba12fff5a6d38469da0aeb9fe
yaikohi <erik.beem@student.hu.nl>
2021-06-16 09:29:36 +0200

    Implemented chartjs in CoinDetails component

--- components/CoinDetails/CoinDetails.tsx
+++ components/CoinDetails/CoinDetails.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Line } from "react-chartjs-2";
+
 
 interface CoinDetailsProps {
-    
+    price: any,
+    data: any,
 }
-
-export const CoinDetails = ({...props}: CoinDetailsProps) => {
+export function CoinDetails({ price, data }: CoinDetailsProps) {
+    const opts = {
+        tooltips: {
+            intersect: false,
+            mode: "index"
+        },
+        responsive: true,
+        maintainAspectRatio: false
+    };
+    if (price === "0.00") {
+        return <h2>please select a currency pair</h2>;
+    }
     return (
-        <div>
-            CoinDetails
+        <div className="dashboard">
+            <h2>{`$${price}`}</h2>
+
+            <div className="chart-container">
+                {/* https://stackoverflow.com/questions/67618984/react-typescript-chart-js-error-type-is-missing-in-type */}
+                <Line type="line" data={data} options={opts} />
+            </div>
         </div>
-    )
+    );
 }
