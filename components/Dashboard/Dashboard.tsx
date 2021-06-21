5e3800de87befbcc8a0b79142aa736a43eb8517a
Erik Beem <erik.beem@hotmail.com>
2021-06-21 14:34:13 +0200

    Updated dashboard

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -2,33 +2,34 @@
 // * for connecting to a coinbase websocket in a react project
 
 // using useRef hooks to update a variable without reloading the component: https://www.smashingmagazine.com/2020/11/react-useref-hook/
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, SyntheticEvent } from "react";
 // import io from "socket.io";
 
 import { CoinList } from "../CoinList/CoinList";
-import { CoinDetails } from "../CoinDetails/CoinDetails";
+import { CoinDetails } from "../CoinChart/CoinChart";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { useSWRFetch } from "../util/fetchData";
 import { formatData } from "../util/formatData";
 import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
+  const CoinGeckoApiUrl =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum%2C%20cardano%2C%20xrp%2C%20polkadot%2C%20litecoin%2C%20dash%2C%20chainlink%2C%20vechain%2C%20tron&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
   // Two different URLS; one for the coinbase API & one for the coinbase WSS
   const apiUrl = "https://api.pro.coinbase.com";
 
-  const CoinGeckoApiUrl =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum%2C%20cardano%2C%20xrp%2C%20polkadot%2C%20litecoin%2C%20dash%2C%20chainlink%2C%20vechain%2C%20tron&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   const wsUrl = "wss://ws-feed.pro.coinbase.com";
 
   // the ref 'first' will prevent the first api call
-  // socket is our websocket reference 
+  // socket is our websocket reference
   let first = useRef(false);
   const socket = useRef(null);
 
-  const [currencies, setcurrencies] = useState([]);
-  const [pair, setpair] = useState("");
-  const [price, setprice] = useState("0.00");
-  const [pastData, setpastData] = useState({});
+  const [currencies, setcurrencies] = useState([]); // all the currencies
+  const [coinId, setCoinId] = useState(""); // the coin to be selected
+  const [price, setprice] = useState("0.00"); // the price of the selected coin
+  const [pastData, setpastData] = useState({}); // for the initial render
 
   const { coins, isLoading, isError } = useSWRFetch(CoinGeckoApiUrl);
 
@@ -37,16 +38,16 @@ export default function Dashboard() {
     let pairs = [];
 
     const fetchPairs = async () => {
-      await fetch(apiUrl + '/products')
+      await fetch(apiUrl + "/products")
         .then((res) => res.json())
-        .then((data) => (pairs = data))
-        console.log(pairs);
+        .then((data) => (pairs = data));
+      console.log(pairs);
 
-      let filtered = pairs.filter((pair) => {
-        if (pair.quote_currency === "EUR") {
-          return pair;
+      let filtered = pairs.filter((coinId) => {
+        if (coinId.quote_currency === "EUR") {
+          return coinId;
         }
-      })
+      });
 
       filtered = filtered.sort((a, b) => {
         if (a.base_currency < b.base_currency) {
@@ -57,32 +58,36 @@ export default function Dashboard() {
         }
         return 0;
       });
-      console.log(filtered)
       setcurrencies(filtered);
-
       first.current = true;
-    }
-
+    };
     fetchPairs();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (!first.current) {
-      console.log('returning to the first render');
+      console.log("returning to the first render");
       return;
     }
     // subscription to the coinbase websocket
+    let currenciesList = [];
+    currencies.map((currency) => {
+      currenciesList.push(currency.id);
+    });
 
-    console.log('running pair change');
+    // console.log(currenciesList)
+
     let wsSubMsg = {
       type: "subscribe",
-      product_ids: ["ADA-EUR", "VET-EUR"],
+      product_ids: currenciesList,
       channels: ["ticker"],
     };
+
     let jsonWsSubMsg = JSON.stringify(wsSubMsg);
+
     socket.current.send(jsonWsSubMsg);
 
-    let historicalDataURL = `${apiUrl}/products/${pair}/candles?granularity=86400`;
+    let historicalDataURL = `${apiUrl}/products/${coinId}/candles?granularity=86400`;
     const fetchHistoricalData = async () => {
       let dataArr = [];
       await fetch(historicalDataURL)
@@ -98,27 +103,27 @@ export default function Dashboard() {
     socket.current.onmessage = (e) => {
       let data = JSON.parse(e.data);
       if (data.type !== "ticker") {
-        console.log('non-ticker event', e)
+        console.log("non-ticker event", e);
         return;
       }
 
-      if (data.product_id === pair) {
+      if (data.product_id === coinId) {
         setprice(data.price);
       }
     };
-  }, [pair]);
+  }, [coinId]);
 
-  const handleSelect = (e) => {
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     let unsubMsg = {
       type: "unsubscribe",
-      product_ids: [pair],
-      channels: ["ticker"]
+      product_ids: [coinId],
+      channels: ["ticker"],
     };
     let unsub = JSON.stringify(unsubMsg);
 
     socket.current.send(unsub);
 
-    setpair(e.target.value);
+    setCoinId(e.target.value);
   };
 
   if (isLoading) return <div>Loading...</div>;
@@ -127,26 +132,31 @@ export default function Dashboard() {
 
   if (coins)
     return (
-      <div className={styles.Dashboard}>
-         <div className="container">
-           {
-             <select name="currency" value={pair} onChange={handleSelect}>
-               {currencies.map((cur, idx) => {
-                 return (
-                   <option key={idx} value={cur.id}>
-                     {cur.display_name}
-                   </option>
-                 );
-               })}
-             </select>
-           }
-           <CoinDetails price={price} data={pastData} />
-         </div>
-         
-        <h2>Crypto-Dashboard</h2>
-        <p>Dashboard</p>
-        <SearchBar />
-        <CoinList coins={coins} />
+      <div className={styles.container}>
+        <div>
+        {
+            <select
+              placeholder="Select a crypto/fiat pair..."
+              name="currency"
+              value={coinId}
+              onChange={handleSelect}
+            >
+              {currencies.map((cur, idx) => {
+                return (
+                  <option key={idx} value={cur.id}>
+                    {cur.display_name}
+                  </option>
+                );
+              })}
+            </select>
+          }
+        </div>
+        <div>
+          <CoinDetails price={price} data={pastData} />
+        </div>
+        <div className={styles.Dashboard}>
+          <CoinList coins={coins} />
+        </div>
       </div>
     );
-  }
+}
