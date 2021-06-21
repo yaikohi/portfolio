24959ffa657e3f8481b33bed1fdeca9022b3e7f5
Erik Beem <erik.beem@hotmail.com>
2021-06-21 14:33:55 +0200

    Removed empty element

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,20 +1,12 @@
 import Dashboard from "../components/Dashboard/Dashboard";
-import { CoinDetails } from "../components/CoinDetails/CoinDetails";
-
-import styles from "../styles/Work.module.css";
 
 export default function Work() {
   return (
     <div>
       <h1> My work </h1>
-      <div> 
-        <CoinDetails />
-      </div>
-
       <div>
         <Dashboard />
       </div>
-
     </div>
   );
 }
